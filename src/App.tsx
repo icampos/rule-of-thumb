521dfbe8eb2f1353d131f12799b834d4db4f3c1d
isaac.camposcruz@gmail.com <isaac.camposcruz@gmail.com>
2023-08-28 16:09:06 -0600

    Adding initial components files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import BannerTop from "./components/BannerTop";
+import BannerBottom from "./components/BannerBottom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Header />
+      <div className="max-centered">
+        <BannerTop />
+        <BannerBottom />
+        <hr role="separator" />
+
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/BannerBottom.tsx
@@ -0,0 +1,31 @@
+import BackgroundPeopleImg from '../assets/img/bg-people.png'
+import BackgroundPeopleImg2x from '../assets/img/bg-people.@2x.png'
+
+const BannerBottom = () => {
+  return (
+    <aside
+      className="banner banner-bottom"
+      role="doc-tip"
+      aria-label="Submit a name"
+    >
+      <img
+        srcSet={`${BackgroundPeopleImg} 750w, ${BackgroundPeopleImg2x} 1440w`}
+        sizes="(min-width: 750px) 1440px, 100vw"
+        className="banner__background"
+        src={BackgroundPeopleImg}
+        alt=""
+        role="none"
+      />
+      <div className="banner__left">
+        <h2 className="banner__heading">
+          Is there anyone else you would want us to add?
+        </h2>
+      </div>
+      <div className="banner__right">
+        <button className="banner__cta">Submit a name</button>
+      </div>
+    </aside>
+  );
+};
+
+export default BannerBottom;
