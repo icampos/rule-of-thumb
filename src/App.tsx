28c74b3fd690df9a531d70a50ad32a471043c873
isaac.camposcruz@gmail.com <isaac.camposcruz@gmail.com>
2023-08-29 17:37:40 -0600

    Adding sass support and initial css for voting card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import BannerTop from "./components/BannerTop";
 import BannerBottom from "./components/BannerBottom";
+import VotingSection from "./components/VotingSection";
+import VotingCard from "./components/VotingCard";
+
+import PopeFrancisImg from './assets/img/pope-francis.png'
+
+const votes = {
+  positive: 1,
+  negative: 1,
+};
 function App() {
   return (
     <div className="App">
@@ -13,6 +22,16 @@ function App() {
       <Header />
       <div className="max-centered">
         <BannerTop />
+        <VotingSection>
+          <VotingCard
+            name="Kanye West"
+            description="Vestibulum diam ante, porttitor a odio eget, rhoncus.  Eu velit…"
+            category="Enterteinment"
+            picture={PopeFrancisImg}
+            lastUpdated=""
+            votes={votes}
+          />
+        </VotingSection>
         <BannerBottom />
         <hr role="separator" />
 
--- src/components/VotingCard.tsx
+++ src/components/VotingCard.tsx
@@ -1,16 +1,17 @@
 import ThumbsUpImg from "../assets/img/thumbs-up.svg";
 import ThumbsDownImg from "../assets/img/thumbs-down.svg";
+import "../styles/VotingCard.scss";
 
 interface VotingCardProps {
-  name: string
-  description: string
-  category: string
-  picture: string
-  lastUpdated: string
+  name: string;
+  description: string;
+  category: string;
+  picture: string;
+  lastUpdated: string;
   votes: {
-    positive: number
-    negative: number
-  }
+    positive: number;
+    negative: number;
+  };
 }
 
 const VotingCard = ({
@@ -22,20 +23,21 @@ const VotingCard = ({
   votes,
 }: VotingCardProps) => {
   return (
-    <>
-      <img
-        className="voting-card__background"
-        src={picture}
-        alt={name}
-      />
-      <div className="voting-card">
+    <div className="voting-card">
+      <img className="voting-card__background" src={picture} alt={name} />
+      <div className="voting-card__content">
         <div className="voting-card__glass-background"></div>
-        <div className="voting-card__content">
-          <h2 className="voting-card__title">{name}</h2>
-          <p className="voting-card__desc">
-            {description}
-          </p>
-          <p className="voting-card__cta">{category}</p>
+          <div className="voting-card__info">
+            <div className="voting-card__result voting-card__positive">
+              <img src={ThumbsUpImg} alt="thumbs up" />
+            </div>
+            <div>
+              <h2 className="voting-card__title">{name}</h2>
+              <p className="voting-card__description">{description}</p>
+            </div>
+          </div>
+          <div className="voting-card__cta">
+          <p className="voting-card__category">{category}</p>
           <div className="voting-card__buttons">
             <button className="icon-button" aria-label="thumbs up">
               <img src={ThumbsUpImg} alt="thumbs up" />
@@ -43,19 +45,24 @@ const VotingCard = ({
             <button className="icon-button" aria-label="thumbs down">
               <img src={ThumbsDownImg} alt="thumbs down" />
             </button>
-            <button aria-label="vote now">Vote Now</button>
+            <button className="vote-button" aria-label="vote now">
+              Vote Now
+            </button>
           </div>
         </div>
-      </div>
-      <div className="voting-gauge">
+        <div className="voting-gauge">
         <div className="voting-gauge__left">
+          <img src={ThumbsUpImg} alt="thumbs up" />
           <span className="voting-gauge__number">{votes.positive}</span>
         </div>
         <div className="voting-gauge__right">
+          <img src={ThumbsDownImg} alt="thumbs down" />
           <span className="voting-gauge__number">{votes.negative}</span>
         </div>
       </div>
-    </>
+      </div>
+
+    </div>
   );
 };
 
