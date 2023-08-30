a3d2bf8787ab67e889ace1a9ffaaf3762bab8425
isaac.camposcruz@gmail.com <isaac.camposcruz@gmail.com>
2023-08-29 20:18:33 -0600

    Adding tablet version styles for voting card

--- src/components/VotingCard.tsx
+++ src/components/VotingCard.tsx
@@ -25,8 +25,9 @@ const VotingCard = ({
   return (
     <div className="voting-card">
       <img className="voting-card__background" src={picture} alt={name} />
+      <div className="voting-card__glass-background"></div>
       <div className="voting-card__content">
-        <div className="voting-card__glass-background"></div>
+        <div className="voting-card__details">
           <div className="voting-card__info">
             <div className="voting-card__result voting-card__positive">
               <img src={ThumbsUpImg} alt="thumbs up" />
@@ -37,31 +38,34 @@ const VotingCard = ({
             </div>
           </div>
           <div className="voting-card__cta">
-          <p className="voting-card__category">{category}</p>
-          <div className="voting-card__buttons">
-            <button className="icon-button" aria-label="thumbs up">
-              <img src={ThumbsUpImg} alt="thumbs up" />
-            </button>
-            <button className="icon-button" aria-label="thumbs down">
-              <img src={ThumbsDownImg} alt="thumbs down" />
-            </button>
-            <button className="voting-card__vote-button" aria-label="vote now">
-              Vote Now
-            </button>
+            <p className="voting-card__category">{category}</p>
+            <div className="voting-card__buttons">
+              <button className="icon-button" aria-label="thumbs up">
+                <img src={ThumbsUpImg} alt="thumbs up" />
+              </button>
+              <button className="icon-button" aria-label="thumbs down">
+                <img src={ThumbsDownImg} alt="thumbs down" />
+              </button>
+              <button
+                className="voting-card__vote-button"
+                aria-label="vote now"
+              >
+                Vote Now
+              </button>
+            </div>
           </div>
         </div>
         <div className="voting-card__gauge">
-        <div className="voting-card__gauge-left">
-          <img src={ThumbsUpImg} alt="thumbs up" />
-          <span className="voting-card__gauge-number">{votes.positive}</span>
-        </div>
-        <div className="voting-card__gauge-right">
-          <img src={ThumbsDownImg} alt="thumbs down" />
-          <span className="voting-card__gauge-number">{votes.negative}</span>
+          <div className="voting-card__gauge-left">
+            <img src={ThumbsUpImg} alt="thumbs up" />
+            <span className="voting-card__gauge-number">{votes.positive}</span>
+          </div>
+          <div className="voting-card__gauge-right">
+            <img src={ThumbsDownImg} alt="thumbs down" />
+            <span className="voting-card__gauge-number">{votes.negative}</span>
+          </div>
         </div>
       </div>
-      </div>
-
     </div>
   );
 };
