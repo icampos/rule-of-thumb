4064930d6a7be11fb2efd3e985d86666d032bc4c
isaac.camposcruz@gmail.com <isaac.camposcruz@gmail.com>
2023-08-31 02:14:00 -0600

    Adding test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import VotingCard from './components/VotingCard';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const person = {
+  id: "1",
+  name: "Pope Francis",
+  description: "The Pope",
+  category: "Religion",
+  image: "https://www.biography.com/.image/t_share/MTQ3NTc2MjU5MjQ0MjUwMzUx/pope_francis_280716_edit.jpg",
+  lastUpdated: "1 month",
+  votes: {
+    positive: 36,
+    negative: 64
+  },
+  voted: false
+}
+
+
+it('should render a disabled vote now button', () => {
+  render(<VotingCard
+    key={person.id}
+    id={person.id}
+    name={person.name}
+    description={person.description}
+    category={person.category}
+    picture={person.image}
+    lastUpdated={person.lastUpdated}
+    votes={person.votes}
+    voted={person.voted}/>)
+
+  const voteNowButton = screen.getByText(/Vote Now/i);
+  expect(voteNowButton).toBeDisabled();
 });
+
+it('should render and enabled vote again button and thanks you message if the card has been voted', () => {
+  render(<VotingCard
+    key={person.id}
+    id={person.id}
+    name={person.name}
+    description={person.description}
+    category={person.category}
+    picture={person.image}
+    lastUpdated={person.lastUpdated}
+    votes={person.votes}
+    voted={true}/>)
+
+  const voteAgainButton = screen.getByText(/Vote Again/i);
+  const thankYouMessage = screen.getByText(/Thank you for your vote!/i);
+
+  expect(voteAgainButton).toBeEnabled();
+  expect(thankYouMessage).toBeInTheDocument();
+});
+
+
